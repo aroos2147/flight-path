0fcc145597522e13d77e655413bd605783a1d101
Arlo <arlo.roos@wsu.edu>
2025-11-17 15:44:44 -0800

    Node loading methods

--- src/constants.py
+++ src/constants.py
@@ -3,6 +3,7 @@
 ######################################################################
 
 from neo4j import GraphDatabase
+from pyspark.sql import SparkSession
 
 NEO4J_URI = "bolt://localhost:7687"
 NEO4J_USER = "neo4j"
@@ -14,3 +15,7 @@ COUNTRIES_PATH = "data/countries.dat"
 ROUTES_PATH = "data/routes.dat"
 
 DRIVER = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
+SPARK = (SparkSession.builder
+            .appName("FlightPath")
+            .config("spark.jars.packages", "org.neo4j:neo4j-connector-apache-spark_2.12:5.3.1")
+            .getOrCreate())
--- src/queries.py
+++ src/queries.py
@@ -2,8 +2,9 @@
 # Methods for executing neo4j queries
 ######################################################################
 
-from constants import DRIVER
+from constants import DRIVER, SPARK, NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD
 
+# Executes a generic query on the neo4j database
 def executeQuery(query):
     with DRIVER.session() as session:
         try:
@@ -11,3 +12,30 @@ def executeQuery(query):
         except Exception as e:
             print("Error executing query:\n")
             print(query)
+
+# Loads all nodes of the given name into a Spark dataframe
+def getNodes(name):
+    return (SPARK.read
+                .format("ne04j")
+                .option("url", NEO4J_URI)
+                .option("authentication.type", "basic")
+                .option("authentication.basic.username", NEO4J_USER)
+                .option("authentication.basic.password", NEO4J_PASSWORD)
+                .option("labels", ":" + name)
+                .load())
+
+# Loads the neo4j Airport nodes into a Spark dataframe
+def getAirports():
+    return getNodes("Airport")
+
+# Loads the neo4j Airline nodes into a Spark dataframe
+def getAirlines():
+    return getNodes("Airline")
+
+# Loads the neo4j Country nodes into a Spark dataframe
+def getCountries():
+    return getNodes("Country")
+
+# Loads the neo4j Route nodes into a Spark dataframe
+def getRoutes():
+    return getNodes("Route")
